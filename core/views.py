a1a062981b379568bc60882716e1b3e124e2d071
lucas souza <n3b8k9c7@gmail.com>
2020-12-11 20:28:05 -0300

    Aula 6 atualização

--- core/views.py
+++ core/views.py
@@ -1,11 +1,32 @@
+from projeto_lucas.settings import AUTH_PASSWORD_VALIDATORS
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, request
 from core.models import Evento
-'''
-def index(request):
-    return redirect('/agenda/')
-'''
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+@login_required(login_url='/login/')
 def listaEventos(request):
+    #user = request.user
     evento = Evento.objects.get(id=1)
     dados = {'evento':evento}
     return render(request,'agenda.html', dados)
+
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, 'Usuário ou senha inválidos')
+    return redirect('/')
